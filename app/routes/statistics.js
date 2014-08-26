094d1ea3c54330bddbef01b77d880226c2ae5bdd
ahlunju <ahlunju@hotmail.com>
2014-08-26 16:17:11 -0400

    swapped dummy data with data from API

--- app/routes/statistics.js
+++ app/routes/statistics.js
@@ -2,11 +2,18 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function () {
+    // var url = 'https://api.github.com/users/ahlunju';
     var developers = [
       { name:'Carlos Ar', login: 'carlitosspot'},
       { name:'Richard Heras', login: 'Richardheras'},
       { name:'David Balogun', login: 'webdev34'}
     ];
+    // var data = Ember.$.getJSON(url).then(function(data) {
+    //   console.log(data);
+    //   developers[developers.length] = data;
+    //   return developers;
+    // });
+    // return data;
     return developers;
   }
 });
