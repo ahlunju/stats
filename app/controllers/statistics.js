6694a6a983ee21ff731cfe4c5ea91852c9f0f39c
ahlunju <ahlunju@hotmail.com>
2014-08-27 16:55:54 -0400

    store added user so don't do anything if enter 2nd time

--- app/controllers/statistics.js
+++ app/controllers/statistics.js
@@ -1,19 +1,27 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
+  addedUsers : [],
   actions : {
     getUserLogin: function() {
-      console.log(this.get('model')); //the statistics model is an array of githubers
-      if (this.get('login')) {
+      var newUser = this.get('login');
+      if (newUser) {
         var model = this.get('model');
         var self = this;
-        var url = 'https://api.github.com/users/'+this.get('login');
-        Ember.$.getJSON(url).then(function(data){
-          self.set('githuber', data);
-          model.pushObject(data); //make API call and add to the list
-        });
-        this.set('login'); //reset
+        var url = 'https://api.github.com/users/'+newUser;
+
+        if (this.addedUsers.indexOf(newUser) === -1) {
+          this.addedUsers.push(newUser); //store the user that has already entered
+          console.log(this.addedUsers);
+          Ember.$.getJSON(url).then(function(data){
+            self.set('githuber', data);
+            model.pushObject(data); //make API call and add to the list
+          });
+          console.log(model);
+        }
+        this.set('login'); //empty the input field
       }
     }
+
   }
 });
